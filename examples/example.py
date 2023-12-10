aff064f64d220cf14f51860268b842f2b1f5d69d
Sam Korn <korn94sam@gmail.com>
2023-12-09 23:28:05 -0700

    add more usage examples for creating a schedule

--- examples/example.py
+++ examples/example.py
@@ -2,63 +2,84 @@ from pprint import pprint
 from requests import post
 
 
-from ishelly.components.switch import SwitchToggleRequest, SwitchToggleParams, SwitchSetRequest, SwitchSetParams
-from ishelly.components.schedule import ScheduleCreateRequest, ScheduleCreateParams, ScheduleListRequest, ScheduleListResponse
+from ishelly.components.switch import *
+# (
+#     SwitchToggleRequest,
+#     SwitchToggleParams,
+#     SwitchSetRequest,
+#     SwitchSetParams,
+# )
+from ishelly.components.schedule import *
+# (
+#     ScheduleCreateRequest,
+#     ScheduleCreateParams,
+#     ScheduleUpdateRequest,
+#     ScheduleUpdateParams,
+#     ScheduleUpdateResponse,
+#     ScheduleListRequest,
+#     ScheduleListResponse,
+# )
 
 device_rpc_url = "http://192.168.1.200/rpc"
 
 switch_id = 0
+SECONDS_IN_HOUR = 3600
 job = SwitchSetRequest(
     id=1,
-    params=SwitchSetParams(id=switch_id, toggle_after=3600, on=True)
+    params=SwitchSetParams(id=switch_id, toggle_after=2 * SECONDS_IN_HOUR, on=True),
 )
 
 ## cron jobs!
 # every 2 hours
-timespec1 = "0 0 */2 * * *"
+timespec1 = "0 0 10 * * *"
 # every 2 hours between 8am and 6pm
-timespec2 = "0 8-18/2 * * * *"
+timespec2 = "0 */8 * * * *"
 
-# create a new scheduled task
+# CREATE NEW scheduled tasks
 req = ScheduleCreateRequest(
-    id=1, 
+    id=1,
     params=ScheduleCreateParams(
-        enable=True, 
-        timespec=timespec1, 
-        calls=[job.model_dump()]
-    )
+        enable=True, timespec=timespec1, calls=[job.model_dump()]
+    ),
 )
 
 response = post(device_rpc_url, json=req.model_dump())
-
+schedule_create_1 = ScheduleCreateResponse(**response.json()["result"])
 
 req = ScheduleCreateRequest(
-    id=1, 
+    id=1,
     params=ScheduleCreateParams(
-        enable=True, 
-        timespec=timespec2, 
-        calls=[job.model_dump()]
-    )
+        enable=True, timespec=timespec2, calls=[job.model_dump()]
+    ),
 )
 
 response = post(device_rpc_url, json=req.model_dump())
+schedule_create_2 = ScheduleCreateResponse(**response.json()["result"])
 
-# update an existing scheduled task
-# req = ScheduleUpdateRequest(
-#     id=1, 
-#     params=ScheduleUpdateParams(
-#         id=1,
-#         enable=False, 
-#         timespec="*/5 * * * * *", 
-#         calls=[job.model_dump()]
-#     )
-# )
-# response = post(device_rpc_url, json=req.model_dump())
+### UPDATE existing scheduled task
+timespec_updated = "0 0 11 * * *"
+req = ScheduleUpdateRequest(
+    id=1,
+    params=ScheduleUpdateParams(
+        id=1, enable=True, timespec=timespec_updated, calls=[job.model_dump()]
+    ),
+)
+response = post(device_rpc_url, json=req.model_dump())
+schedule_update = ScheduleUpdateResponse(**response.json()["result"])
 
 
-# List scheduled tasks
+### LIST scheduled tasks
 req = ScheduleListRequest(id=1)
 response = post(device_rpc_url, json=req.model_dump())
 scheduled_tasks = ScheduleListResponse(**response.json()["result"])
 
 print(f"There are {len(scheduled_tasks.jobs)} scheduled tasks currently saved.")
+
+### DELETE a scheduled task
+req = ScheduleDeleteRequest(
+    id=1,
+    params=ScheduleDeleteParams(id=3),
+)
+response = post(device_rpc_url, json=req.model_dump())
+schedule_delete = ScheduleDeleteResponse(**response.json()["result"])
+
